204edbcd0ccfc5c8423483f34faeb02eae9f8568
Huy Phạm <Huy17vn>
2018-07-29 03:31:03 +0700

    huy

---
+++ includes/menu.php
@@ -0,0 +1,81 @@
+
+ <div class="wapper-h">
+        <div class="left-area">
+            <ul>
+                <li class="toolbar-customer">
+                    <a href="/account/login">
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                        Login
+                    </a>
+                </li>
+                <li class="toolbar-customer">
+                    <a href="/pages/wish-list">
+                        <i class="fa fa-heart" aria-hidden="true"></i>
+                        Wishlist
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="right-area">
+            <div class="nav-search">
+                <form class="search" action="/search">
+                    <input type="text" name="txtsearch" class="search-box" placeholder="Enter your keyword ...">
+                    <button class="search-submit" type="submit">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="nav-cart">
+                <img class="cart-img" src="img/bg-cart.png" alt="...">
+            </div>
+        </div>
+    </div>
+    <div class="nav-menu">
+        <div class="navigation-left">
+            <ul class="navigation-links">
+                <li class="nav-item">
+                    <a href="index.php">
+                        <img src="img/m-flowers.png" alt="flowers" class="img">
+                        <span>Flowers</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/pages/contact">
+                        <img src="img/m-support.png" alt="support" class="img">
+                        <span>Support</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/blogs/news">
+                        <img src="img/m-blog.png" alt="blog" class="img">
+                        <span>Blog</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="mn-logo">
+        	<img src="img/m-Logo.png" alt="">
+        </div>
+        <div class="navigation-right">
+            <ul class="navigation-links">
+                <li class="nav-item">
+                    <a href="/">
+                        <img src="img/m-about.png" alt="About" class="img">
+                        <span>About</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/pages/contact">
+                        <img src="img/m-contact.png" alt="Contact" class="img">
+                        <span>Contact</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="upload.php">
+                        <img src="img/m-faq.png" alt="Upload" class="img">
+                        <span>Upload</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
